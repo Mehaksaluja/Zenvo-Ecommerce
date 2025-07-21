18da065ef1d6c63b3909dbe549ff016c7b88eeb9
Mehaksaluja <salujamehak11@gmail.com>
2025-07-21 21:46:06 +0530

    Route for products

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
 const app = express();
 dotenv.config()
 connectDB();
@@ -10,6 +11,8 @@ app.get("/", (req, res) => {
     msg: "Server is running."
   })
 })
+
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`App is running on port ${PORT}`));

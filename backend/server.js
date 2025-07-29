5f5d320227298659d8198923df0293e6b8d6dcd9
Mehaksaluja <salujamehak11@gmail.com>
2025-07-29 15:48:55 +0530

    changes saved

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path';
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
@@ -12,11 +13,18 @@ connectDB();
 app.use(express.json());
 
 app.use(cors());
-app.get("/", (req, res) => {
-  res.json({
-    msg: "Server is running."
-  })
-})
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);

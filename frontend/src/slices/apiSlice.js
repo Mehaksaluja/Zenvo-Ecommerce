57b629f251aa1ff3ddc16c5a83c195139954e51b
Mehaksaluja <salujamehak11@gmail.com>
2025-07-30 11:46:46 +0530

    changes saved

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -2,11 +2,16 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: '',
+  // This prepareHeaders function runs before every request
   prepareHeaders: (headers, { getState }) => {
+    // Get the userInfo object from our Redux auth state
     const { userInfo } = getState().auth;
+
+    // If userInfo and its token exist, add the token to the headers
     if (userInfo && userInfo.token) {
       headers.set('authorization', `Bearer ${userInfo.token}`);
     }
+
     return headers;
   },
 });
